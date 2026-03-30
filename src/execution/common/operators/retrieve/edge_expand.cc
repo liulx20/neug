f749c75bf7cb850542af278ed10cfb4576443409
liulx20 <519459125@qq.com>
2026-03-30 17:54:42 +0800

    fix ci

--- src/execution/common/operators/retrieve/edge_expand.cc
+++ src/execution/common/operators/retrieve/edge_expand.cc
@@ -69,6 +69,9 @@ neug::result<Context> EdgeExpand::expand_degree(
   }
   foreach_vertex(*vertex_col, [&](size_t index, label_t label, vid_t v) {
     int64_t degree = 0;
+    if (v == graph.kInvalidVid) {
+      return;
+    }
     if (mps.count(label)) {
       for (auto& view : mps.at(label)) {
         auto es = view.get_edges(v);
